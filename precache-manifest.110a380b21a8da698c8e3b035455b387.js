daab41de9f13c392c9ea797ba6f349cc4a4493cd
UtopiaLab <89520@fhss.sjp.ac.lk>
2021-08-26 21:36:40 +0530

    Updates

---
+++ precache-manifest.110a380b21a8da698c8e3b035455b387.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53fef080fddbc40b5d507d745f4779d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e373c0ce175c4cf9ddbb",
+    "url": "/static/css/main.a90790e1.chunk.css"
+  },
+  {
+    "revision": "3c470dd094b9fe2c68af",
+    "url": "/static/js/2.8955f365.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.8955f365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e373c0ce175c4cf9ddbb",
+    "url": "/static/js/main.a3ada4c4.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "0b686ecdc3084ccbb2578abd7d185717",
+    "url": "/static/media/CV_UmayangaMadushan_v3.2.0b686ecd.pdf"
+  },
+  {
+    "revision": "3391f34be33095dcbc6a2745818b912a",
+    "url": "/static/media/bmv.3391f34b.jpg"
+  },
+  {
+    "revision": "ad22ed3237394256e10e9f944bad093f",
+    "url": "/static/media/fiverr.ad22ed32.jpg"
+  },
+  {
+    "revision": "e66244bfadab5126e33cd12bf7a25a0b",
+    "url": "/static/media/kcc.e66244bf.jpg"
+  },
+  {
+    "revision": "8a37c1c4ba0526f2e4f88bbce610aa20",
+    "url": "/static/media/sjp.8a37c1c4.jpg"
+  },
+  {
+    "revision": "5055719cbdef18eb5803c490cbf65176",
+    "url": "/static/media/slt.5055719c.jpg"
+  },
+  {
+    "revision": "ed34edd5501cac17510a48716c22b98c",
+    "url": "/static/media/umayanga.ed34edd5.jpg"
+  }
+]);
